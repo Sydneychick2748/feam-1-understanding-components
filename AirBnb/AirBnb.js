ca229fb610c20ec43969cba2899eec6ecf145591
ana <ana@anniecannons.com>
2023-11-13 13:17:48 -0800

    commit airbnb

--- AirBnb/AirBnb.js
+++ AirBnb/AirBnb.js
@@ -1,29 +1,35 @@
 /**
  Using the image of the Airbnb UI, answer the following questions:
  1. What are four main components you see on the page?
-
-
+nav bar 
+search bar 
+filters 
+cards 
 
 
 
  2. Put the components in order to show the parent/child relationship. So if component x is the mother of y and grandmother of z, you would write: 
-    1. x
-    2. y
-    3. z
+    1. x - search bar 
+    2. y - filter 
+    3. z - cards
 
 
 
 
 3. Which component needs access to every piece of data? Which component only needs access to the filtered data?
 
-
-
+ 
+search bar will need all the data and the cards will only need the filtered data 
 
 
 
 
 4. What are three different key/value pairs that will be needed for each listing?
 
+location 
+views 
+date 
+price 
 
 
 
@@ -33,3 +39,6 @@
 
 5. What JavaScript could we write to filter the objects based on whether or not the listing has a pool?
  */
+// Assuming you have an array of listing objects
+// Filter listings that have a pool
+// Filter listings that do not have a pool
